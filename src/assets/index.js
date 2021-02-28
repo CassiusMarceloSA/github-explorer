e039d754ae7d3070ce2d393b43dd38a527f34756
cassius.avila <cassius.avila@edu.pucrs.br>
2021-02-28 14:26:53 -0300

    :sparkles: added Logo and search icon

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,3 +2,5 @@ export { default as GitHubIcon } from "./icons/GitHubIcon";
 export { default as GoToLinkIcon } from "./icons/GoToLinkIcon";
 export { default as StarIcon } from "./icons/StarIcon";
 export { default as ReposIcon } from "./icons/ReposIcon";
+export { default as SearchIcon } from "./icons/SearchIcon";
+export { default as Logo } from "./Logo";
