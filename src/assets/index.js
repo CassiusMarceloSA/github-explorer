c40aadffa6b1cf360b50f832b8d29cc954b0ddbb
cassius.avila <cassius.avila@edu.pucrs.br>
2021-02-27 22:42:57 -0300

    🍱 added some icons

---
+++ src/assets/index.js
@@ -0,0 +1,4 @@
+export { default as GitHubIcon } from "./icons/GitHubIcon";
+export { default as GoToLinkIcon } from "./icons/GoToLinkIcon";
+export { default as StarIcon } from "./icons/StarIcon";
+export { default as ReposIcon } from "./icons/ReposIcon";
