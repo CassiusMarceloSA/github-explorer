a112e4911da52dce55dd6017f6d74d3a12c543a2
cassius.avila <cassius.avila@edu.pucrs.br>
2021-03-01 01:44:10 -0300

    :mag: improve SEO

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,14 @@
+import { Head } from "next/document";
 import Header from "../containers/Header";
 import { UserContext } from "../context";
 import "../styles/globals.scss";
 function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>Github Explorer</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Header />
       <UserContext.Provider>
         <Component {...pageProps} />
