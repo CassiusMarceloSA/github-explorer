23e6e4e990acd21dddc604ea20b6c83b0e023ea7
cassius.avila <cassius.avila@edu.pucrs.br>
2021-03-01 01:21:38 -0300

    :mag: improve SEO

---
+++ src/pages/_document.js
@@ -0,0 +1,22 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="pt-BR">
+        <Head>
+          <title>Github Explorer</title>
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+          <link rel="shortcut icon" href="favicon.png" type="image/png" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
